1fba78b36aa457992046f06f45e27a8ef655fc38
Tom <64384849+tnierodzik@users.noreply.github.com>
2020-09-12 20:20:53 -0400

    First Edititon

--- UFO-Level-1/static/JS/app.js
+++ UFO-Level-1/static/JS/app.js
@@ -1,4 +1,22 @@
 // from data.js
 var tableData = data;
 
+var tbody = d3.select("tbody");
+
+
+
 // YOUR CODE HERE!
+// console.log(data);
+
+tableData.forEach(function(UFOsightings) {
+  console.log(UFOsightings);
+  var row = tbody.append("tr");
+  Object.entries(UFOsightings).forEach(function([key, value]) {
+    console.log(key, value);
+    // Append a cell to the row for each value
+    // in the weather report object
+    var cell = row.append("td");
+    cell.text(value);
+  });
+});
+
